e2a0e68840304f3764e9cca64c0c2f1d34d64be9
Admin <admin@example.com>
2026-07-16 14:14:10 +0700

    fix: change const DB_FILE to let DB_FILE to prevent TypeError

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const DB_FILE = path.join(__dirname, "db.json");
+let DB_FILE = path.join(__dirname, "db.json");
 
 const initialData = {
     employees: {
